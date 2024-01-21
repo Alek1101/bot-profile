9b331a1f0fe9ce1f9c10bac3b2148eb5084336b9
Alek1101 <kevinalek10@gmail.com>
2024-01-21 18:58:38 +0300

    info

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import telebot
 from telebot import types
+from telebot.types import ReplyKeyboardMarkup
 import os
 from dotenv import load_dotenv
+from functions import save_progress, load_progress
+from game import survey
 
 load_dotenv()
 token = os.getenv('API_KEY')
 if token is None:
     raise ValueError('в файле .env отсутствует API_KEY')
-bot = telebot.TeleBot('token')
+bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(commands=['start'])
@@ -19,64 +22,71 @@ def start(message):
 @bot.message_handler(commands=['help'])
 def start(message):
     bot.send_message(message.chat.id, 'Тут есть команды:'
-                                      '\n1. /start запуск самого бота'
-                                      '\n2. /help узнать возможности бота'
-                                      '\n3. /start_test запустить анкету')
+                                      '\n1. /start - запуск бота'
+                                      '\n2. /help - узнать возможности бота'
+                                      '\n3. /start_game - запустить анкету'
+                                      '\n4. /end - остановить игру')
+
+
+question = -1
 
 
 def base_questions(m: types.Message):
     global question
-    message = m.text
-    if message == '/end':
-        bot.send_message(m.chat.id, 'Прохождение остановлено',
-                         reply_markup=types.ReplyKeyboardRemove())
-        return
     user_id = m.from_user.id
+    message = m.text
+    if survey[question]['choice'] == -1:
+        bot.send_message(m.chat.id, 'Игра окончена.'
+                                    '\nСпасибо за прохождение!', reply_markup=types.ReplyKeyboardRemove())
+        save_progress(user_id, -1)
+        return
+    if message == '/end':
+        print('end')
+        bot.send_message(m.chat.id, 'Прохождение прекращено',
+                         reply_markup=types.ReplyKeyboardRemove())
+        question = -1
+        save_progress(user_id, -1)
+        return
     try:
-        for season, scores in survey[question]["answers"][message].items():
-            total_score[season] += scores
-        user_data[user_id] = dict(user_score=total_score)
-        question += 1
-        if question >= len(survey):
-            question = 0
-            bot.send_message(m.chat.id, 'Итоги анкеты:', reply_markup=types.ReplyKeyboardRemove())
-            max_score = 0
-            your_season = ''
-            for season, scores in total_score.items():
-                bot.send_message(m.chat.id, f"{season} - {scores}")
-                if scores > max_score:
-                    max_score = scores
-                    your_season = season
-            print(user_data)
-            for count in total_score:
-                total_score[count] = 0
-            bot.send_message(m.chat.id, f"Наибольшее количество баллов у времени года: {your_season}")
+
+        question = survey[question]['choice'][message]
+
+        if survey[question]['choice'] == -1:
+            print('-1, end')
+            bot.send_message(m.chat.id, survey[question]['text'])
             return
         markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-        for key in survey[question]['answers'].keys():
+        for key in survey[question]['choice'].keys():
             markup.add(key)
-        bot.send_message(m.chat.id, survey[question]["question"], reply_markup=markup)
+        bot.send_message(m.chat.id, survey[question]["text"], reply_markup=markup)
         bot.register_next_step_handler(m, base_questions)
     except:
-        bot.reply_to(m, 'Что-то сломалось... Попробуйте ещё раз')
+        print('exc')
+        bot.send_message(m.chat.id, 'Введите ответ с клавиатуры:')
         markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-        for key in survey[question]['answers'].keys():
+        for key in survey[question]['choice'].keys():
             markup.add(key)
-        bot.send_message(m.chat.id, survey[question]["question"], reply_markup=markup)
+        bot.send_message(m.chat.id, survey[question]["text"], reply_markup=markup)
         bot.register_next_step_handler(m, base_questions)
 
 
 @bot.message_handler(commands=['start_game'])
 def profile_output(m: types.Message):
+    global question
     bot.send_message(m.from_user.id, 'Запускаем игру')
     user_id = m.from_user.id
-    user_name = m.from_user.first_name
-
+    question = load_progress(user_id)
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    for key in survey[question]['answers'].keys():
-        markup.add(key)
-    bot.send_message(m.chat.id, survey[question]["question"], reply_markup=markup)
+    if question != -1 and question is not None:
+        for key in survey[question]['answers'].keys():
+            markup.add(key)
+        bot.send_message(m.chat.id, survey[question]["text"], reply_markup=markup)
+    if question == -1 and question is not None:
+        bot.send_message(m.chat.id, survey[question]['text'])
+    else:
+        bot.send_message(m.chat.id, survey[-1]['text'])
+        question = 0
     bot.register_next_step_handler(m, base_questions)
 
 
-bot.polling()
+bot.infinity_polling()
